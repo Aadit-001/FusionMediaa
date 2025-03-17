30cad1019e8cf9d72cd78f9079a31ccd0419622f
Aadit Jha <developer.aj.001@gmail.com>
2025-03-17 20:02:40 +0530

    ds

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { RootLayout } from './RootLayout'
-import Work from './pages/Work'
+import Work from './pages/Work.jsx'
 import Services from './pages/Services'
-import Clients from './pages/Clients'
+import Clients from './pages/Clients.jsx'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Work from './pages/work.jsx'
-import Clients from './pages/clients.jsx'
+import Work from './pages/Work.jsx'
+import Clients from './pages/Clients.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
